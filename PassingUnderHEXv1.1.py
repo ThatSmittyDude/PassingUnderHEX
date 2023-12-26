4cda2cf5e69e33a78417eb685466cf61ad1b2db5
that_smitty_dude <ThatSmittyDude@outlook.com>
2023-12-26 13:09:59 -0500

    Add files via upload

---
+++ PassingUnderHEXv1.1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#PassingUnderHEXv1.1
+
+"""
+Created on Tue Dec 26 11:36:07 2023
+
+@author:    ThatSmittyDude@outlook.com
+            puyinside.com
+            TikTok @ that_smitty_dude
+"""
+
+while True:
+    # Binary
+    print("========================================================")
+    print("| 1024 | 512 | 256 | 128 | 64 | 32 | 16 | 8 | 4 | 2 | 1|")
+    print("========================================================")
+    print("PassingUnderHexv1.1")
+    print(" ")
+    print(" ")
+    print(hex(int(input("enter decimal to convert to HEX: "))))
+    print(" ")
+    print(" ")
+    
+
---
+++ PassingUnderHEXv1.2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#PassingUnderHEXv1.2
+
+"""
+Created on Tue Dec 26 11:36:07 2023
+
+@author:    ThatSmittyDude@outlook.com
+            puyinside.com
+            TikTok @ that_smitty_dude
+"""
+
+while True:
+    # Binary
+    print("=============================================================")
+    print("| F | E | D | C | B | A | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 |")
+    print("=============================================================")
+    print("PassingUnderHexv1.2")
+    print(" ")
+    print(" ")
+    print(hex(int(input("enter decimal to convert to HEX: "))))
+    print(" ")
+    print(" ")
+    
+
